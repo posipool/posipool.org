59daaf8b66d95c1fe330b41bce6be73d6dea77d3
Josias Junior <josias.junior@quintoandar.com.br>
2022-11-17 22:22:32 -0300

    Create posipool logo component

--- src/components/Logo/index.tsx
+++ src/components/Logo/index.tsx
@@ -1,20 +1,19 @@
-import Image from "next/image"
-import styled from "styled-components"
-import styles from "../../styles/Home.module.css"
+import Image from 'next/image'
+import styled from 'styled-components'
+import styles from '../../styles/Home.module.css'
 
-const Brand = styled.img``
+const LogoContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`
 
 export default function Logo() {
   return (
-    // <span className={styles.logo}>
-    <main className={styles.logo}>
-      <Image
-        src="/posipool.svg"
-        alt="Posi Pool Logo"
-        width={465.65}
-        height={118.65}
-      ></Image>
-    </main>
-    // </span>
+    <LogoContainer>
+      <Image src={'/posipool-logo-oficial.svg'} alt="Posi brand" width={65} height={65} />
+      <Image src="/posipool.svg" alt="Posi Pool Logo" width={300} height={100} className={styles.image}></Image>
+    </LogoContainer>
   )
 }
