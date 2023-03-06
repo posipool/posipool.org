10a1615c6a24b4a0506bb3a263f3827d0900fa9d
Josias Junior <1001josias@gmail.com>
2023-03-05 21:22:49 -0300

    Implement roulette smart contract

--- src/components/LuckyWheelWithButtonAndPanel/index.tsx
+++ src/components/LuckyWheelWithButtonAndPanel/index.tsx
@@ -7,6 +7,7 @@ import { Web3Button } from '@thirdweb-dev/react';
 import style from "./style.module.css"
 import Image from "next/image"
 import styled from "styled-components"
+import { SmartContract } from '@thirdweb-dev/sdk';
 
   const LogoContainer = styled.div`
     z-index: 999;
@@ -19,12 +20,9 @@ import styled from "styled-components"
   `
 
   export default function LuckyWheelWithButtonAndPanel (props: WheelProps) {
-    const [prize, setPrize] = useState<number>()
     const myLucky = React.useRef<LuckyWheel>(null);
+    const [ isSpinning, setIsSpinning ] = useState(false)
     const { reward, isAnimating } = useReward('rewardId', 'confetti', {zIndex: 9999999, lifetime: 200, angle: 85, onAnimationComplete: ()=> {console.log('completou')}  });
-    useEffect(()=> {
-      setPrize((Math.random() * 6) >> 0)
-    }, [isAnimating])
     const background = "#0865a1"
     const buttons = [
       { radius: "30px", background: "rgb(0, 53, 87)" },
@@ -37,34 +35,45 @@ import styled from "styled-components"
       },
     ];
 
-    const prizesName = ['Lose', '1 Posi', 'Lose', '2 Posi', '+1 spin', '3 Posi', 'Lose', '4 Posi', '+2 spin', '8 Posi']
+    const prizesName = ['Lose', '1 Posi', 'Lose', '2 Posi', '+1 Spin', '3 Posi', 'Lose', '4 Posi', '+2 Spin', '8 Posi']
     const  fontSize = 20
-
+    
     const background2 = "white"
     const prizes = prizesName.map((prizeName, index) => {
       return (index % 2)? { background, imgs: [{src: '/moneybag2.png', top: 50, width: 80}], fonts: [{ text: prizeName, top: 25, fontColor: 'white', fontSize }], } :
       { background: background2, fonts: [{ text: prizeName, top: 25, fontColor:  (prizeName ==='Lose')? '#3d0710':'#0865a1', fontSize }] }
     })
 
-    function onSuccess() {
+    function onSuccess(resultado: number) {
       setTimeout(() => {
-        myLucky.current?.stop(4);
+        myLucky.current?.stop(resultado);
       }, 5000);
-      console.log('success')
     }
 
-    function onStart() {
+    function spinRoulette(resultado:number) {
+      console.log('start spin')
+      setIsSpinning(true)
       myLucky.current?.play();
-      onSuccess()
-      console.log('start')
+      onSuccess(resultado)
     }
 
     function onEnd(prize: LuckWheelPrize) {
-      console.log(prize.fonts[0].text)
       console.log('Sorteado: ' + prize.fonts[0].text)
       prize.fonts[0].text != 'Lose' && reward()
+      setIsSpinning(false)
     }
-return <>
+
+    async function callRouletteContract(contract: SmartContract) {
+      // Executar chamada do contract aqui
+      const result = await contract.call('spin')
+      const prize = result.receipt.events[1].args.prize.name
+      console.log({prize})
+      console.log(prizesName.indexOf(prize))
+      console.log(result)
+      // const prize = await dummyContractCall(1000)
+      spinRoulette(prizesName.indexOf(result.receipt.events[1].args.prize.name))
+      }
+  return <>
     <Div id="rewardId">
     <LogoContainer>
       <Image src={'/posipool-shadow.png'} alt="Posi brand" width={65} height={65} />
@@ -78,40 +87,35 @@ return <>
         height="350px"
         prizes={prizes}
         buttons={buttons}
-        onStart={onStart}
-        onEnd={(a:any)=> {
-          onEnd(a)
-        }} 
+        // onStart={onStart}
+        onEnd={onEnd}
         />
 
         <Web3Button
+        isDisabled={isSpinning}
         className={style['spin-button']}
         accentColor="#0a93eb"
-        onSuccess={(contract)=> {
-          console.log(contract)
-        }}
-        // onSubmit={()=> console.log('chamada enviada')}
+        onSuccess={(contract)=> {}}
+        onSubmit={()=> console.log('chamada enviada')}
         contractAddress={props.contract.address}
         contractAbi={props.contract.abi}
-        action={async (contract) => {
-          console.log({contract})
-          // Executar chamada do contract aqui
-          await dummyContractCall(1000)
-          onStart()
+        action={callRouletteContract}
+          onError={(error)=> {
+            console.log(error)
+            setIsSpinning(false)
           }}
-          onError={(error)=> console.log(error)}
         >Spin</Web3Button>
         </Div>
   </>
   }
   
   async function dummyContractCall(time: number) {
-    const result = Math.random()
-    return new Promise((resolve, reject) =>
+    const result = Math.floor((Math.random() * 10))
+    return new Promise<number>((resolve, reject) =>
     setTimeout(() => {
       if(result < 0.5){
-        console.log(result,'testando');
-        resolve(resolve)
+        console.log(result);
+        resolve(result)
       }else{
         reject(result + ' falhou')
       }
---
+++ src/components/SardineRoulette/contract.ts
@@ -0,0 +1,296 @@
+const address: string = '0x9F72BFC848179f2b0a02048031F494ee4cEC7674'
+const abi = [
+  {
+    "inputs": [
+      {
+        "internalType": "uint16",
+        "name": "_ticketPrice",
+        "type": "uint16"
+      }
+    ],
+    "stateMutability": "nonpayable",
+    "type": "constructor"
+  },
+  {
+    "anonymous": false,
+    "inputs": [
+      {
+        "indexed": true,
+        "internalType": "string",
+        "name": "action",
+        "type": "string"
+      },
+      {
+        "indexed": true,
+        "internalType": "address",
+        "name": "player",
+        "type": "address"
+      },
+      {
+        "indexed": false,
+        "internalType": "uint256",
+        "name": "timestamp",
+        "type": "uint256"
+      },
+      {
+        "indexed": false,
+        "internalType": "uint256",
+        "name": "blocknumber",
+        "type": "uint256"
+      }
+    ],
+    "name": "ActionLog",
+    "type": "event"
+  },
+  {
+    "anonymous": false,
+    "inputs": [
+      {
+        "indexed": true,
+        "internalType": "address",
+        "name": "player",
+        "type": "address"
+      },
+      {
+        "components": [
+          {
+            "internalType": "string",
+            "name": "name",
+            "type": "string"
+          },
+          {
+            "internalType": "uint8",
+            "name": "weight",
+            "type": "uint8"
+          },
+          {
+            "internalType": "uint8",
+            "name": "posis",
+            "type": "uint8"
+          },
+          {
+            "internalType": "uint8",
+            "name": "tickets",
+            "type": "uint8"
+          }
+        ],
+        "indexed": false,
+        "internalType": "struct Lukywheel.Prize",
+        "name": "prize",
+        "type": "tuple"
+      }
+    ],
+    "name": "RewardLog",
+    "type": "event"
+  },
+  {
+    "anonymous": false,
+    "inputs": [
+      {
+        "indexed": false,
+        "internalType": "address",
+        "name": "a",
+        "type": "address"
+      },
+      {
+        "indexed": false,
+        "internalType": "uint256",
+        "name": "b",
+        "type": "uint256"
+      }
+    ],
+    "name": "vrflog",
+    "type": "event"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "uint16",
+        "name": "_numTickets",
+        "type": "uint16"
+      }
+    ],
+    "name": "buyTickets",
+    "outputs": [],
+    "stateMutability": "payable",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "getContractBalance",
+    "outputs": [
+      {
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "address",
+        "name": "_player",
+        "type": "address"
+      }
+    ],
+    "name": "getPlayer",
+    "outputs": [
+      {
+        "components": [
+          {
+            "internalType": "uint16",
+            "name": "tickets",
+            "type": "uint16"
+          },
+          {
+            "internalType": "uint16",
+            "name": "balance",
+            "type": "uint16"
+          },
+          {
+            "internalType": "uint16",
+            "name": "games",
+            "type": "uint16"
+          },
+          {
+            "internalType": "uint16",
+            "name": "wins",
+            "type": "uint16"
+          },
+          {
+            "internalType": "bool",
+            "name": "blocked",
+            "type": "bool"
+          },
+          {
+            "internalType": "address",
+            "name": "user",
+            "type": "address"
+          }
+        ],
+        "internalType": "struct Lukywheel.Player",
+        "name": "",
+        "type": "tuple"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "address",
+        "name": "_player",
+        "type": "address"
+      },
+      {
+        "components": [
+          {
+            "internalType": "uint16",
+            "name": "tickets",
+            "type": "uint16"
+          },
+          {
+            "internalType": "uint16",
+            "name": "balance",
+            "type": "uint16"
+          },
+          {
+            "internalType": "uint16",
+            "name": "games",
+            "type": "uint16"
+          },
+          {
+            "internalType": "uint16",
+            "name": "wins",
+            "type": "uint16"
+          },
+          {
+            "internalType": "bool",
+            "name": "blocked",
+            "type": "bool"
+          },
+          {
+            "internalType": "address",
+            "name": "user",
+            "type": "address"
+          }
+        ],
+        "internalType": "struct Lukywheel.Player",
+        "name": "_info",
+        "type": "tuple"
+      }
+    ],
+    "name": "setPlayerInfo",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "uint16",
+        "name": "_newPrice",
+        "type": "uint16"
+      }
+    ],
+    "name": "setTicketPrice",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "spin",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "spinCount",
+    "outputs": [
+      {
+        "internalType": "uint16",
+        "name": "",
+        "type": "uint16"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "ticketPrice",
+    "outputs": [
+      {
+        "internalType": "uint16",
+        "name": "",
+        "type": "uint16"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "uint256",
+        "name": "_amount",
+        "type": "uint256"
+      }
+    ],
+    "name": "withdraw",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  }
+]
+
+export default {
+  address,
+  abi
+}
