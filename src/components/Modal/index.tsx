b31361e855ed1ed36676052384475a250d3ed609
Josias Junior <1001josias@gmail.com>
2023-03-14 08:25:05 -0300

    Implement the modal component (#44)

---
+++ src/components/Modal/index.tsx
@@ -0,0 +1,30 @@
+import {
+  Modal as ChakraModal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalBody,
+  ModalCloseButton,
+  Center,
+} from "@chakra-ui/react";
+import { ModalProps } from "./types";
+
+export default function Modal(props: ModalProps) {
+  const {children, title, ...rest} = props
+  return (
+    <>
+      <ChakraModal {...rest} isCentered>
+        <ModalOverlay />
+        <ModalContent>
+          <Center>
+            <ModalHeader>{title}</ModalHeader>
+          </Center>
+          <ModalCloseButton />
+            <ModalBody>
+              {children}
+            </ModalBody>
+        </ModalContent>
+      </ChakraModal>
+    </>
+  )
+}
---
+++ src/components/Modal/types.d.ts
@@ -0,0 +1,5 @@
+import { ModalProps as chakraModalProps } from "@chakra-ui/react"
+
+interface ModalProps extends chakraModalProps {
+    title: string
+}
