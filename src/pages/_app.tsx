9a9c8a09658cf57f2e3f8fd255a717f5ad8e2f8c
Josias Junior <1001josias@gmail.com>
2023-03-27 21:29:02 -0300

    Remove the styled-components provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,6 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { GlobalStyle } from './style'
-import { ThemeProvider } from 'styled-components'
-// import { theme } from '../theme'
 import { Poppins } from '@next/font/google'
 import { ThirdwebProvider } from "@thirdweb-dev/react";
 import { PosichainShard0, HarmonyShard0 } from '@thirdweb-dev/chains'
@@ -16,7 +14,6 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <main className={poppins.className}>
       <GlobalStyle />
-      <ThemeProvider theme={theme}>
         <ThirdwebProvider 
       supportedChains={[]}
        activeChain={PosichainShard0}
@@ -33,7 +30,6 @@ export default function App({ Component, pageProps }: AppProps) {
             <Component {...pageProps} />
           </ChakraProvider>
         </ThirdwebProvider>
-      </ThemeProvider>
     </main>
   )
 }
