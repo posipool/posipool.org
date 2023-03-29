6776241377358f31387c92c0041d2f5e6b0f4c61
Josias Junior <1001josias@gmail.com>
2023-03-28 22:24:20 -0300

    Set gradient background color (#65)

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,6 +1,16 @@
 import { extendTheme, ChakraTheme } from "@chakra-ui/react"
 
 const customTheme: Partial<ChakraTheme> = {
+  styles: {
+    global: {
+      "html, body": {
+        bgGradient: "linear(to-t, blue.900, gray.900)",
+        height: "100%",
+        width: "100%",
+        overflowY: 'auto'
+      }
+    }
+  },
   config: {
     initialColorMode: "dark",
   }
