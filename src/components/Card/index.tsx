c1a35e99bd184449cf58c6ed4cff975619baf0a6
Josias Junior <1001josias@gmail.com>
2023-04-29 13:40:53 -0300

    card component

---
+++ src/components/Card/index.tsx
@@ -0,0 +1,12 @@
+import { CardProps, Card as ChackraCard } from '@chakra-ui/react'
+
+export default function Card(props: CardProps) {
+  return (
+    <ChackraCard
+      {...props}
+      backgroundColor={props.backgroundColor || 'blackAlpha.600'}
+      borderColor={props.borderColor || 'gray.600'}
+      borderWidth={props.borderWidth || 1}
+    ></ChackraCard>
+  )
+}
